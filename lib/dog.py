3ce32b66406f2be5479f89935194b4b4da3de490
Frankline Were <franklinegift@gmail.com>
2024-09-18 11:03:05 +0300

    translation

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,42 @@
 from models import Dog
 
-def create_table(base):
-    pass
+
+def create_table(base, engine):
+    base.metadata.create_all(engine)
+
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
+
 
 def get_all(session):
-    pass
+    dogs = session.query(Dog).all()
+    return dogs
+
 
 def find_by_name(session, name):
-    pass
+    name = session.query(Dog).filter(Dog.name == name).first()
+    return name
+
 
 def find_by_id(session, id):
-    pass
+    id = session.query(Dog).filter(Dog.id == id).first()
+    return id
+
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    name_breed = (
+        session.query(Dog).filter((Dog.name == name), (Dog.breed == breed)).first()
+    )
+    return name_breed
+
 
 def update_breed(session, dog, breed):
-    pass
+    breed_update = session.query(Dog).filter(Dog.id == dog.id).first()
+
+    if breed_update:
+        breed_update.breed = breed
+        session.commit()
+    else:
+        raise ValueError("No dog found with that id")
